012a23a6cd466b00c9437287aa0cd57a71009110
Adil Jamoun <ajamoun@c1r6p5.1337.ma>
2025-08-14 14:26:22 +0100

    MINISHELL

--- pars/ft_strtok.c
+++ pars/ft_strtok.c
@@ -12,5 +12,28 @@ t_lexer *ft_init_lexer(char *input)
 }
 t_lexer *ft_strtok(char *input, t_list *minienv)
 {
-    
+    t_lexer *lexer;
+    t_token *token;
+    t_token *token_list;
+    bool    heredoc;
+
+    if(!input)
+        return (NULL);
+    lexer = ft_init_lexer(input);
+    // head of linked list of tokens
+    token_list = NULL;
+    // state flag: are we inside a heredoc?
+    heredoc = false;
+    token = ->> ft_get_next_token();<<-
+    while(token)
+    {
+        if(!token)
+            break ;
+        if(token->value)
+            ->>ft_tokadd_back(, ->>ft_newtok()<<-);<<-
+        ->>ft_free_token();<<-
+        token = ->>ft_get_next_token();<<-
+    }
+    free(token);
+    return (token_list);
 }
