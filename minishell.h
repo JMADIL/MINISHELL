11c0014ccc31dba60a08af89810e4aae255b193b
Adil Jamoun <ajamoun@c1r6p7.1337.ma>
2025-08-22 04:26:58 +0100

    MINISHELL

--- minishell.h
+++ minishell.h
@@ -6,7 +6,7 @@
 /*   By: ajamoun <ajamoun@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/07 02:12:45 by ajamoun           #+#    #+#             */
-/*   Updated: 2025/08/08 05:53:33 by ajamoun          ###   ########.fr       */
+/*   Updated: 2025/08/22 04:26:26 by ajamoun          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -95,7 +95,7 @@ typedef struct s_cmdarg
 	t_redi_list			*output;
 	struct s_cmdarg		*next;
 }						t_cmdarg;
-
+//this one should be on the libft.h
 typedef struct s_list
 {
 	char			*key;
@@ -104,5 +104,65 @@ typedef struct s_list
 	struct s_list	*prev;
 	struct s_list	*next;
 }					t_list;
+
+
+bool	ft_redierrors(t_token	*token);
+bool	ft_pipeerrors(t_token *token);
+void	expand_norm(t_token **token, t_list	*minienv, ssize_t dpos);
+void	expand_exit_status(t_token **token);
+void	expand_variables(t_token **token, t_list *minienv);
+bool	ft_condition(t_token **token, ssize_t dollar_pos);
+char	*ft_strjoin_free(char *s1, char *s2);
+char	*ft_expand(t_token **token, t_list	*minienv, ssize_t dollat_pos);
+char	*ft_getenv(char *var, t_list *minienv);
+char	*ft_getvar(char *var, t_list *minienv);
+int	ft_get_varlen(char *str);
+char	*help_tmp2(t_token **token, ssize_t dollar_pos);
+bool	please_expand(t_token *tmp);
+void	ft_free_token(t_token *token);
+void	ft_free_tokenlist(t_token *token_list);
+void	ft_free_cmdlist(t_cmdarg *cmdarg_list);
+t_lexer *ft_init_lexer(char *input);
+t_lexer *ft_strtok(char *input, t_list *minienv);
+void	ft_init_fields(t_token *token);
+void	ft_set_token_type(t_token token, int op_len, char *op, bool *heredoc);
+t_token	*handling_operator(t_lexer lexer, bool heredoc);
+t_token	*handling_quotes(t_lexer *lexer, char quote);
+t_token	*handling_words(t_lexer *lexer);
+t_list	*ft_initenv(char **env);
+t_cmdarg	*ft_init_node(void);
+t_cmdarg	*get_next_node(t_token	*token_list);
+t_cmdarg	*ft_newnode(t_cmdarg *node);
+void	ft_nodeadd_back(t_cmdarg **lst, t_cmdarg *new);
+bool	resize_cmd_array(t_cmdarg **node, int new_capacity);
+int	ft_pars_env(char *env_line, char **key, char **value);
+static int	extend_cmd_for_word(t_cmdarg **node);
+static void	add_or_join_word(t_cmdarg **node, t_token *token_list, char *value);
+void	*parsing_word(t_cmdarg	**node, t_token *token_list);
+void	*parsing_redi(t_cmdarg **node, t_token *token_list);
+int	no_need_chars(char *input);
+bool ft_quote_checker(char *input);
+bool	ft_isredi(t_token *token);
+t_redi_list	*ft_redinew(t_token *token, bool expand);
+void	ft_rediradd(t_redi_list **redi, t_redi_list *new);
+void	join_or_add_first_word(t_cmdarg **node, char **split_words,
+		bool first_word_joins);
+void	extend_cmd_array_if_needed(t_cmdarg **node, int needed_capacity);
+void	add_split_words(t_cmdarg **node, char **split_words,
+		bool first_word_joins);
+void	ft_free_split_words(char **split_words);
+void	handle_split_word(t_cmdarg **node, t_token *token_list, char *value);
+t_token	*ft_dollar_joining(t_token *token, t_token *new_token);
+int	ft_toksize(t_token *lst);
+t_token	*newtok(t_token *token);
+void	ft_tokadd_back(t_token **token_list, t_token *token);
+bool	ft_tojoin(t_lexer *lexer);
+t_token *ft_jointok(t_token *token, t_lexer **lexer, t_list *minienv, bool *heredoc);
+t_token	*get_next_token(t_lexer *lexer, t_list *minienv, bool heredoc);
+int ft_isspace(char c);
+int	ft_isspecial(char c);
+ssize_t	dollar_position(char *str);
+bool	ft_is_cmd(t_token *current);
+
 
 #endif
--- pars/ft_free.c
+++ pars/ft_free.c
@@ -6,7 +6,7 @@
 /*   By: ajamoun <ajamoun@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/17 05:01:20 by ajamoun           #+#    #+#             */
-/*   Updated: 2025/08/21 06:14:22 by ajamoun          ###   ########.fr       */
+/*   Updated: 2025/08/22 04:18:58 by ajamoun          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,18 +43,5 @@ void	ft_free_cmdlist(t_cmdarg *cmdarg_list)
 		tmp = cmdarg_list;
 		cmdarg_list = cmdarg_list->next;
 		ft_free_node(tmp);
-	}
-}
-
-void	ft_free_tokenlist(t_token *token_list)
-{
-	t_token	*tmp;
-
-	while (token_list)
-	{
-		tmp = token_list;
-		token_list = token_list->next;
-		free(tmp->value);
-		free(tmp);
 	}
 }
